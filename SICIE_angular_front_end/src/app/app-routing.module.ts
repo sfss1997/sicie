66116448c0735f98b8e7feefa8e62431394b9d40
DiegoVgz <32312428+DiegoVgz@users.noreply.github.com>
2019-11-03 12:20:48 -0600

    Login and recinto select menu
    the recinto is  almost  finished, it just need some changes and  the recinto select menu is finished too

--- SICIE_angular_front_end/src/app/app-routing.module.ts
+++ SICIE_angular_front_end/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { MenuRecintoComponent } from './menu-recinto/menu-recinto.component';
+
+
 
 const routes: Routes = [
   {path: 'login' , component: LoginComponent},
+  {path: 'recinto' , component: MenuRecintoComponent},
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- SICIE_angular_front_end/src/app/app.module.ts
+++ SICIE_angular_front_end/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MenuRecintoComponent } from './menu-recinto/menu-recinto.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MenuRecintoComponent
   ],
   imports: [
     BrowserModule,
