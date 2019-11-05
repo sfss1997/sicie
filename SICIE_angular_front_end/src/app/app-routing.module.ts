149b79750f9046c7d915c97e92da3f34a4a36c53
DiegoVgz <32312428+DiegoVgz@users.noreply.github.com>
2019-11-04 21:48:20 -0600

    coordination teachers module table
    added the table to this module but it does not have order by option, it have a filter

--- SICIE_angular_front_end/src/app/app-routing.module.ts
+++ SICIE_angular_front_end/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MenuRecintoComponent } from './menu-recinto/menu-recinto.component';
 import { MenuCoordinacionComponent } from './menu-coordinacion/menu-coordinacion.component';
 import { NavbarGeneralComponent } from './navbar-general/navbar-general.component';
+import { MenuCoordinacionDocentesComponent } from './menu-coordinacion-docentes/menu-coordinacion-docentes.component';
 
 
 
@@ -13,7 +14,7 @@ const routes: Routes = [
   {path: 'recinto' , component: MenuRecintoComponent},
   {path: 'coordinacion' , component:  MenuCoordinacionComponent},
   {path: '' , component:  NavbarGeneralComponent},
- 
+  {path: 'docentes', component: MenuCoordinacionDocentesComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- SICIE_angular_front_end/src/app/app.module.ts
+++ SICIE_angular_front_end/src/app/app.module.ts
@@ -10,7 +10,46 @@ import { CoordinacionDocentesEditarComponent } from './coordinacion-docentes-edi
 import { CoordinacionDocentesRegistrarComponent } from './coordinacion-docentes-registrar/coordinacion-docentes-registrar.component';
 import { NavbarGeneralComponent } from './navbar-general/navbar-general.component';
 import { NavbarCoordinacionComponent } from './navbar-coordinacion/navbar-coordinacion.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {HttpClientModule} from '@angular/common/http';
+import {CdkTableModule} from '@angular/cdk/table';
 
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
+  MatFormFieldModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -24,11 +63,57 @@ import { NavbarCoordinacionComponent } from './navbar-coordinacion/navbar-coordi
     NavbarGeneralComponent,
     NavbarCoordinacionComponent
   ],
+  exports: [
+    CdkTableModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatTableModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+platformBrowserDynamic().bootstrapModule(AppModule);
