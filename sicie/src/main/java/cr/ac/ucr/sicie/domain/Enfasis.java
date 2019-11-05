069ab5f83e8fef47ba4b9f894dacec2b056c81c4
sfss1997 <sergioss1997@gmail.com>
2019-11-05 00:32:28 -0600

    dominio

--- sicie/src/main/java/cr/ac/ucr/sicie/domain/Enfasis.java
+++ sicie/src/main/java/cr/ac/ucr/sicie/domain/Enfasis.java
@@ -1,9 +1,46 @@
+package cr.ac.ucr.sicie.domain;
+
 import java.util.*;
 
 public class Enfasis {
 
-	private Collection<Curso> cursos;
-	private int idEnfasis;
-	private int nombreEnfasis;
+    private List<Curso> cursos;
+    private int idEnfasis;
+    private int nombreEnfasis;
 
+    public Enfasis() {
+    }
+
+    public Enfasis(List<Curso> cursos, int idEnfasis, int nombreEnfasis) {
+        this.cursos = cursos;
+        this.idEnfasis = idEnfasis;
+        this.nombreEnfasis = nombreEnfasis;
+    }
+
+    public List<Curso> getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(List<Curso> cursos) {
+        this.cursos = cursos;
+    }
+
+    public int getIdEnfasis() {
+        return idEnfasis;
+    }
+
+    public void setIdEnfasis(int idEnfasis) {
+        this.idEnfasis = idEnfasis;
+    }
+
+    public int getNombreEnfasis() {
+        return nombreEnfasis;
+    }
+
+    public void setNombreEnfasis(int nombreEnfasis) {
+        this.nombreEnfasis = nombreEnfasis;
+    }
+
+    
+    
 }
--- sicie/src/main/java/cr/ac/ucr/sicie/domain/PlanDeEstudio.java
+++ sicie/src/main/java/cr/ac/ucr/sicie/domain/PlanDeEstudio.java
@@ -1,13 +1,88 @@
+package cr.ac.ucr.sicie.domain;
+
+import java.io.File;
 import java.util.*;
 
 public class PlanDeEstudio {
 
-	private Collection<Curso> cursos;
-	private String codigo;
-	private String nombreCarrera;
-	private int añoResolucion;
-	private File documentoFundamentacionCarrera;
-	private boolean vigente;
-	private File resolucionDeCreacionDeCarrera;
+    private List<Curso> cursos;
+    private String codigo;
+    private String nombreCarrera;
+    private int anoResolucion;
+    private File documentoFundamentacionCarrera;
+    private boolean vigente;
+    private File resolucionDeCreacionDeCarrera;
+
+    public PlanDeEstudio() {
+    }
+
+    public PlanDeEstudio(List<Curso> cursos, String codigo, String nombreCarrera, int anoResolucion, File documentoFundamentacionCarrera, boolean vigente, File resolucionDeCreacionDeCarrera) {
+        this.cursos = cursos;
+        this.codigo = codigo;
+        this.nombreCarrera = nombreCarrera;
+        this.anoResolucion = anoResolucion;
+        this.documentoFundamentacionCarrera = documentoFundamentacionCarrera;
+        this.vigente = vigente;
+        this.resolucionDeCreacionDeCarrera = resolucionDeCreacionDeCarrera;
+    }
+
+    public List<Curso> getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(List<Curso> cursos) {
+        this.cursos = cursos;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombreCarrera() {
+        return nombreCarrera;
+    }
+
+    public void setNombreCarrera(String nombreCarrera) {
+        this.nombreCarrera = nombreCarrera;
+    }
+
+    public int getAnoResolucion() {
+        return anoResolucion;
+    }
+
+    public void setAnoResolucion(int anoResolucion) {
+        this.anoResolucion = anoResolucion;
+    }
+
+    public File getDocumentoFundamentacionCarrera() {
+        return documentoFundamentacionCarrera;
+    }
+
+    public void setDocumentoFundamentacionCarrera(File documentoFundamentacionCarrera) {
+        this.documentoFundamentacionCarrera = documentoFundamentacionCarrera;
+    }
+
+    public boolean isVigente() {
+        return vigente;
+    }
+
+    public void setVigente(boolean vigente) {
+        this.vigente = vigente;
+    }
+
+    public File getResolucionDeCreacionDeCarrera() {
+        return resolucionDeCreacionDeCarrera;
+    }
+
+    public void setResolucionDeCreacionDeCarrera(File resolucionDeCreacionDeCarrera) {
+        this.resolucionDeCreacionDeCarrera = resolucionDeCreacionDeCarrera;
+    }
+
+    
+    
 
 }
